df9381eb3442500c483ac6fa677c3832807b7a03
manny agbayani <manny.i.agbayani@gmail.com>
2019-01-23 19:56:10 +1300

    add react/babel CDN

---
+++ public/index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+    render() {
+        return(
+                <div>
+                   <h1> Lottery Information 2.0 </h1>
+                </div>
+            )
+        }
+    };
+
+
+ReactDOM.render(<div><App /></div>,document.getElementById("reactBinding"))
+
